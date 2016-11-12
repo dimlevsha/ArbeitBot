6ba57e5c2eb5260652f57bedd0e692e7059ca111
dimlevsha <dimlevsha@gmail.com>
2016-11-13 01:58:54 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -35,7 +35,7 @@ bot.on('message', (msg) => {
    bot.on('message', function (msg) {
       var chatId = msg.chat.id;
       // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-      var photo = "BQADAgADAQAD1EGCD2z1sAdETsBxAg";
+      var photo = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg";
       bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
     });
   
