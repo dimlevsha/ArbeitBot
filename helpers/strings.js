ce3d316fbee42f4ca70727ae19c1916b3bef56b2
dimlevsha <dimlevsha@gmail.com>
2016-11-13 00:14:26 +1000

    Update strings.js

--- helpers/strings.js
+++ helpers/strings.js
@@ -88,7 +88,7 @@ module.exports = {
     '0₽ – 50₽', '50₽ – 250₽', '250₽ – 500₽',
     '500₽ – 1000₽', '1000₽ – 5000₽',
     ],
-  hourlyRateAllRatesOption: 'Vse hourly',
+  hourlyRateAllRatesOption: 'Отправить всем',
   selectedCategory: '✅ ',
   selectedHourlyRate: '✅ ',
   selectedLanguage: '✅ ',
