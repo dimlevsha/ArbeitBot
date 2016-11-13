89648e259968380fe4297d6645753ce320b911cd
dimlevsha <dimlevsha@gmail.com>
2016-11-13 12:45:13 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -30,8 +30,14 @@ bot.on('message', (msg) => {
   else if (!msg.from.username) {
     profile.sendAskForUsername(bot, msg);
     return;
+    
   }
-
+  
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    console.log(msg);
+    bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
+});
   
   profile.textInputCheck(msg, (isTextInput, user) => {
     if (user) {
