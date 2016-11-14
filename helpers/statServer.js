8bc45063458613232025ad459ba77b65a2c020ac
dimlevsha <dimlevsha@gmail.com>
2016-11-14 13:51:49 +1000

    Update statServer.js

--- helpers/statServer.js
+++ helpers/statServer.js
@@ -9,6 +9,10 @@
 const dbmanager = require('./dbmanager');
 const http = require('http');
 
+setInterval(function() {
+    http.get("http://jobmaker.herokuapp.com/getStats");
+}, 300000); // every 5 minutes (300000)
+
 http.createServer((req, res) => {
   if (String(req.url) === '/getStats') {
     dbmanager.userCount()
@@ -41,6 +45,6 @@ http.createServer((req, res) => {
     res.writeHead(404);
     res.end('404');
   }
-}).listen(3000, () => {
+}).listen(process.env.PORT || 5000, () => {
   console.log('Statistics server listening on: 3000'); // eslint-disable-line no-console
 });
