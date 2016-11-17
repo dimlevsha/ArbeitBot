2c4030a6c8a4b890e9bf34538c83153e7651c9ab
dimlevsha <dimlevsha@gmail.com>
2016-11-18 01:11:20 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -83,31 +83,6 @@ bot.onText(/правила/, msg => {
 
 
 
-bot.on('message', function onMessage(msg) {
-   var options = {
-     reply_markup: {
-       inline_keyboard: [[{
-         text: 'Do something',
-         callback_data: 'my_action_fancy_action'
-       }]]
-     }
-   };
- 
-   bot.sendMessage(msg.from.id, 'Message', options);
- });
- 
- bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-   console.log('<< Callback: %j', callbackQuery);
- 
-   let action = callbackQuery.data;
- 
-   var options = {
-     chat_id: callbackQuery.message.chat.id,
-     message_id: callbackQuery.message.message_id
-   };
- 
-   bot.editMessageText('Edited message!', options);
- }); 
 
 /**
  * Fired when user clicks button on inlline keyboard
