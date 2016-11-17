f40436f951a5004490db0ddaec56ece90a0651b8
dimlevsha <dimlevsha@gmail.com>
2016-11-18 01:18:11 +1000

    Update keyboards.js

--- helpers/keyboards.js
+++ helpers/keyboards.js
@@ -32,7 +32,7 @@ const clientKeyboard = [
 ];
 
 const helpKeyboard = [
-  [{ text: 'Связаться с администратором', callback_data: 'my_action_fancy_action' }],
+  [{ text: 'Правила', callback_data: 'pravila_inst' }],
    [{ text: 'Связаться с администратором', url: 'https://telegram.me/dsl1500' }],
   [{ text: 'Благодарим за код', url: 'https://github.com/arbeitbot/arbeitbot' }],
  
