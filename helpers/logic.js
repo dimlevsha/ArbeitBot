c862b4c31f4c3f4beaa047fbda53f182bcf431d1
dimlevsha <dimlevsha@gmail.com>
2016-11-17 20:42:32 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -72,43 +72,6 @@ bot.onText(/создать/, msg => {
   return bot.sendMessage(fromId, sozdat);
 });
 
-// On commands
-bot.on(['/start', '/back'], msg => {
-  
-  let markup = bot.keyboard([
-    ['/buttons', '/inlineKeyboard'],
-    ['/start','/hide']
-  ], { resize: true });
-  
-  return bot.sendMessage(msg.from.id, 'Keyboard example.', { markup });
-
-});
-
-// Buttons
-bot.on('/buttons', msg => {
-
-  let markup = bot.keyboard([
-    [bot.button('contact', 'Your contact'), bot.button('location', 'Your location')],
-    ['/back', '/hide']
-  ], { resize: true });
-
-  return bot.sendMessage(msg.from.id, 'Button example.', { markup });
-
-});
-
-// Hide keyboard
-bot.on('/hide', msg => {
-  return bot.sendMessage(
-    msg.from.id, 'Hide keyboard example. Type /back to show.', { markup: 'hide' }
-  );
-});
-
-// On location on contact message
-bot.on(['location', 'contact'], (msg, self) => {
-  return bot.sendMessage(msg.from.id, `Thank you for ${ self.type }.`);
-});
-
-// Inline buttons
 bot.on('/inlineKeyboard', msg => {
 
   let markup = bot.inlineKeyboard([
@@ -121,29 +84,6 @@ bot.on('/inlineKeyboard', msg => {
   ]);
 
   return bot.sendMessage(msg.from.id, 'Inline keyboard example.', { markup });
-
-});
-
-// Inline button callback
-bot.on('callbackQuery', msg => {
-  // User message alert
-  return bot.answerCallback(msg.id, `Inline button callback: ${ msg.data }`, true);
-});
-
-// Inline query
-bot.on('inlineQuery', msg => {
-
-  const query = msg.query;
-  const answers = bot.answerList(msg.id);
-
-  answers.addArticle({
-    id: 'query',
-    title: 'Inline Query',
-    description: `Your query: ${ query }`,
-    message_text: 'Click!'
-  });
-
-  return bot.answerQuery(answers);
 
 });
 /**
