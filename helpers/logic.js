739f281e58a669adc398795c179d8125f9a9c736
dimlevsha <dimlevsha@gmail.com>
2016-11-22 20:59:12 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -34,7 +34,7 @@ bot.onText(/\/ios/, function (msg) {
   
 bot.onText(/\/android/, function (msg) {
   var fromId = msg.from.id;
-  let andro = "BQADAgADFGwTAAGeGnkEAuURgPysKJYC";
+  let andro = "BQADBAADMgADvTrTBqWwrzDjz2m-Ag";
   bot.sendDocument(fromId, andro);
    bot.sendMessage(fromId, '1. Скачайте полученный файл(нажмите на иконку со стрелочкой)\n2.Нажмите на три точки в правом верхнем углу сообщения.\n3.В появившемся окне выберите "Apply localization file", затем в списке языков найдите нужный. ');
  });
