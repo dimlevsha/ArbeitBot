d4fec1943b297f2ccbd59b0710bc2cc20cca3db2
dimlevsha <dimlevsha@gmail.com>
2016-12-02 12:49:00 +1000

    Update logic.js

--- helpers/logic.js
+++ helpers/logic.js
@@ -191,7 +191,7 @@ function handleKeyboard(msg) {
     keyboards.sendClientMenu(bot, msg.chat.id);
   } else if (text === mainMenuOptions.help) {
     keyboards.sendHelp(bot, msg.chat.id); 
-    bot.sendPhoto(fromId, 'https://pp.vk.me/c637231/v637231601/20f2b/BnhGpwsb-4I.jpg');
+    bot.sendPhoto(msg.chat.id, 'https://pp.vk.me/c637231/v637231601/20f2b/BnhGpwsb-4I.jpg');
   } else if (text === mainMenuOptions.chooseLanguage) {
     languagePicker.sendInterfaceLanguagePicker(bot, msg.chat.id);
   } else if (text === clientOptions.postNewJob) {
